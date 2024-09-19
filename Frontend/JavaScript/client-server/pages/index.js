777fe15e867966721e2d03b495b9d3633d35e8b3
MinnaDeVerdier <minna.deverdier@gmail.com>
2024-09-19 11:27:39 +0200

    website in node.js. Supposedly working, but seem to have made two dockerfiles. Need to check which one is necessary and if the images/container is able to run

---
+++ Frontend/JavaScript/client-server/pages/index.js
@@ -0,0 +1,57 @@
+let inputTextField = document.querySelector("#textInput")
+let display = document.querySelector("#display")
+let inputButton = document.querySelector("#inputButton")
+
+//inputButton.addEventListener("click", ()=>{ readDataFromUser(); })
+inputButton.addEventListener("click", ()=>{ getDataFromUser(); })
+
+// 1 Läs från textfil till display
+let writeDataFromFile =()=>
+{
+    fetch("./files/savefile.txt")
+    .then( (response) => 
+    {
+        return response.text()
+        console.log("response: ",response.text)
+    })
+    .then( (dataItem) =>
+    {
+        display.innerHTML = dataItem
+        inputTextField.value = dataItem
+        console.log("dataitem: ", dataItem)
+    })
+    .catch( (error) =>
+    {
+        console.log(error)
+    })
+}
+
+// 2 Skriv till textfil från input
+let getDataFromUser =()=>
+{
+    let aText = inputTextField.value
+    fetch("./", {
+        method: "post",
+        headers: {
+            "Content-Type": "text/plain"
+            },
+        body: aText
+    })
+    .then( (response) =>
+    {
+        return response.text()
+    })
+    .then( (dataItem) =>
+    {
+        display.innerText = "Done"
+        console.log(dataItem)
+    })
+    .catch( (error) => { console.log(error)})
+}
+
+// läser in vad som skrivs i inputfield
+let readDataFromUser = () =>
+{
+    let recievedData = inputTextField.value;
+    display.textContent=recievedData;
+}
---
+++ Frontend/JavaScript/client-server/server/server.js
@@ -0,0 +1,59 @@
+// let oHttp = require('http');
+// let url = require('url')
+// let filesystem = require('fs')
+
+// //Skapa http-server som aktiveras genom > node "filnamn.etx" i cmd
+// oHttp.createServer( (aRequest, aResponse) =>{
+//         filesystem.readFile('info.html', (err, data) => {
+//                 if(err) return console.error(err)
+//                 aResponse.writeHead(200, {'Content-Type': 'text/html'});
+//                 aResponse.write(data);
+//                 aResponse.end();        
+//             })
+//         }).listen(8080);
+// })
+
+import oExpress from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import bodyParser from 'body-parser';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = oExpress();
+const port = 3000;
+
+//include folders in running server
+app.use('/css', oExpress.static(path.join(__dirname, '../css')));
+app.use(oExpress.static(path.join(__dirname, '../pages')));
+
+//app.use(oExpress.json())
+
+app.use(bodyParser.text({ type: 'text/plain'}))
+
+// app.get("/", (aRequire, aResponse) =>
+// {
+//     aResponse.sendFile("./pages/index.html", { root: __dirname })
+// })
+
+app.post("/", (aRequire, aResponse) =>
+{
+    writeToFile(aRequire.body)
+    console.log("POSTED!!!")
+    console.log(aRequire.body)
+})
+
+import oFileStream from 'fs';
+let writeToFile = (aText) =>
+{
+    oFileStream.writeFile("../files/recievedText.txt", aText, (err) =>
+    {
+        if(err) return console.error(err)
+        else console.log("Data written to file....")
+    })
+}
+
+app.listen(port, () => {
+    console.log("server running at http://localhost:%s", port)
+})
