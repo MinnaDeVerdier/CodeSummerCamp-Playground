e8d830f8047896e2a1480383d0d2f1ccde6a642e
MinnaDeVerdier <minna.deverdier@gmail.com>
2024-09-25 12:06:44 +0200

    added text editor with code styling to frontend. checks code formatting for basic code (open brackets etc) and JS-code. need to fix monaco code stylings for python. NOT tested in container!

--- Frontend/JavaScript/client-server/pages/index.js
+++ Frontend/JavaScript/client-server/pages/index.js
@@ -1,57 +1,57 @@
-let inputTextField = document.querySelector("#textInput")
-let display = document.querySelector("#display")
-let inputButton = document.querySelector("#inputButton")
+// let inputTextField = document.querySelector("#textInput")
+// let display = document.querySelector("#display")
+// let inputButton = document.querySelector("#inputButton")
 
-//inputButton.addEventListener("click", ()=>{ readDataFromUser(); })
-inputButton.addEventListener("click", ()=>{ getDataFromUser(); })
+// //inputButton.addEventListener("click", ()=>{ readDataFromUser(); })
+// inputButton.addEventListener("click", ()=>{ getDataFromUser(); })
 
-// 1 Läs från textfil till display
-let writeDataFromFile =()=>
-{
-    fetch("./files/savefile.txt")
-    .then( (response) => 
-    {
-        console.log("response: ",response.text)
-        return response.text()
-    })
-    .then( (dataItem) =>
-    {
-        display.innerHTML = dataItem
-        inputTextField.value = dataItem
-        console.log("dataitem: ", dataItem)
-    })
-    .catch( (error) =>
-    {
-        console.log(error)
-    })
-}
+// // 1 Läs från textfil till display
+// let writeDataFromFile =()=>
+// {
+//     fetch("./files/savefile.txt")
+//     .then( (response) => 
+//     {
+//         console.log("response: ",response.text)
+//         return response.text()
+//     })
+//     .then( (dataItem) =>
+//     {
+//         display.innerHTML = dataItem
+//         inputTextField.value = dataItem
+//         console.log("dataitem: ", dataItem)
+//     })
+//     .catch( (error) =>
+//     {
+//         console.log(error)
+//     })
+// }
 
-// 2 Skriv till textfil från input
-let getDataFromUser =()=>
-{
-    let aText = inputTextField.value
-    fetch("./", {
-        method: "post",
-        headers: {
-            "Content-Type": "text/plain"
-            },
-        body: aText
-    })
-    .then( (response) =>
-    {
-        return response.text()
-    })
-    .then( (dataItem) =>
-    {
-        display.innerText = "Done"
-        console.log(dataItem)
-    })
-    .catch( (error) => { console.log(error)})
-}
+// // 2 Skriv till textfil från input
+// let getDataFromUser =()=>
+// {
+//     let aText = inputTextField.value
+//     fetch("./", {
+//         method: "post",
+//         headers: {
+//             "Content-Type": "text/plain"
+//             },
+//         body: aText
+//     })
+//     .then( (response) =>
+//     {
+//         return response.text()
+//     })
+//     .then( (dataItem) =>
+//     {
+//         display.innerText = "Done"
+//         console.log(dataItem)
+//     })
+//     .catch( (error) => { console.log(error)})
+// }
 
-// läser in vad som skrivs i inputfield
-let readDataFromUser = () =>
-{
-    let recievedData = inputTextField.value;
-    display.textContent=recievedData;
-}
+// // läser in vad som skrivs i inputfield
+// let readDataFromUser = () =>
+// {
+//     let recievedData = inputTextField.value;
+//     display.textContent=recievedData;
+// }
