7122bd087c25bf2b22c28eb3fbfa4d42d0deca9b
MinnaDeVerdier <minna.deverdier@gmail.com>
2024-09-25 12:03:02 +0200

    modified some styles for frontend. added footer. need to fix the bindings file for easier stylings

--- Frontend/JavaScript/client-server/server/server.js
+++ Frontend/JavaScript/client-server/server/server.js
@@ -1,18 +1,3 @@
-// let oHttp = require('http');
-// let url = require('url')
-// let filesystem = require('fs')
-
-// //Skapa http-server som aktiveras genom > node "filnamn.etx" i cmd
-// oHttp.createServer( (aRequest, aResponse) =>{
-//         filesystem.readFile('info.html', (err, data) => {
-//                 if(err) return console.error(err)
-//                 aResponse.writeHead(200, {'Content-Type': 'text/html'});
-//                 aResponse.write(data);
-//                 aResponse.end();        
-//             })
-//         }).listen(8080);
-// })
-
 import oExpress from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -20,36 +5,34 @@ import path from 'path';
 import bodyParser from 'body-parser';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(dirname(__filename));
 const app = oExpress();
 const port = 3000;
 
 //include folders in running server
-app.use('/css', oExpress.static(path.join(__dirname, '../css')));
-app.use(oExpress.static(path.join(__dirname, '../pages')));
-app.use(oExpress.static(path.join(__dirname, '../files')));
-
-
-//app.use(oExpress.json())
-
+app.use('/css', oExpress.static(path.join(__dirname, 'css')));
+app.use(oExpress.static(path.join(__dirname, 'pages')));
+app.use(oExpress.static(path.join(__dirname, 'files')));
+app.use('/images', oExpress.static(path.join(__dirname, 'images')));
+app.use('/node_modules', oExpress.static(path.join(__dirname, 'node_modules')));
 app.use(bodyParser.text({ type: 'text/plain'}))
 
-// app.get("/", (aRequire, aResponse) =>
-// {
-//     aResponse.sendFile("./pages/index.html", { root: __dirname })
+// app.get('/', (req, res) => {
+//     res.sendFile("./pages/index.html", { root: __dirname })
 // })
 
-app.post("/", (aRequire, aResponse) =>
-{
-    writeToFile(aRequire.body)
-   // console.log("POSTED!!!")
-    console.log(aRequire.body)
+// app.get(`/${page}`, (req, res) => {
+//     res.sendFile(`/files/instruction-${page}.txt`)
+// })
+
+app.post("/", (req, res) => {
+    writeToFile(req.body)
+    console.log(req.body)
 })
 
-import oFileStream from 'fs';
-let writeToFile = (aText) =>
-{
-    oFileStream.writeFile("files/recievedText.txt", aText, (err) =>
+import filestream from 'fs';
+let writeToFile = (aText) => {
+    filestream.writeFile("files/recievedText.txt", aText, (err) =>
     {
         if(err) return console.error(err)
         else console.log("Data written to file....")
