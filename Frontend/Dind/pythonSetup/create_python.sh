19e814813b08ebb3e605cf16f2739a8699cc88c7
Hannes Paulsson <metalamonite@gmail.com>
2024-10-03 10:14:45 +0200

    Gjort filen 'create_python.sh' som kör kommandona man behöver för att skapa python containern inuti Dind.

---
+++ Frontend/Dind/pythonSetup/create_python.sh
@@ -0,0 +1,7 @@
+#cd pythonSetup, om man inte kör filen i mappen redan
+docker image build -t python_test_image .
+#docker image ls
+docker run --mount type=bind,source=/usr/src/app/codefiles,target=/usr/src/app/codefiles,readonly -it --detach --name python_container python_test_image
+#docker container ps -a
+docker container start python_container
+docker exec -it python_container sh
