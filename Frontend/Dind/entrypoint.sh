07c6bd6581ec01c63b4d9aed16f0aee8ceeb2757
MinnaDeVerdier <minna.deverdier@gmail.com>
2024-10-07 18:16:45 +0200

    FIX: check if docker is running before proceding, instead of 60 sec wait. Needs to be tested

--- Frontend/Dind/entrypoint.sh
+++ Frontend/Dind/entrypoint.sh
@@ -5,13 +5,15 @@ echo "...STARTING DOCKERD..."
 # Start the Docker daemon in the background
 /usr/local/bin/dockerd-entrypoint.sh dockerd &
 
-# Wait for the Docker daemon to start BROKEN???
-# while ! docker info >/dev/null 2>&1; do
-echo "...WAITING for Docker daemon to start...\n"
-sleep 60
-#done
+# Wait for the Docker daemon to start. Redirects output to logs, and "2>1" writes errors to same as output
+while ! docker info >>/usr/src/app/logs/createDockerLog.log 2>&1; do
+    echo "...WAITING for Docker daemon to start..."
+    sleep 1
+done
 
 echo "...OPENING LISTENER..."
-# Run your custom script
+
+# Runs listener-script to start watching shared folder
 sh /usr/src/app/listener.sh
+
 echo "...ENTRYPOINT END..."
