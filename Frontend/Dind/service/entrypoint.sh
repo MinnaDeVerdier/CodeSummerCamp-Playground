9e1d71bf932ebe96c8fb1b8078bf592729540a93
MinnaDeVerdier <minna.deverdier@gmail.com>
2024-10-10 19:29:50 +0200

    refactor startup scripts into service and added cleaner on start and every 24 hours

--- Frontend/Dind/service/entrypoint.sh
+++ Frontend/Dind/service/entrypoint.sh
@@ -5,15 +5,17 @@ echo "...STARTING DOCKERD..."
 # Start the Docker daemon in the background
 /usr/local/bin/dockerd-entrypoint.sh dockerd &
 
-# Wait for the Docker daemon to start. Redirects output to logs, and "2>1" writes errors to same as output
-while ! docker info >>/usr/src/app/logs/createDockerLog.log 2>&1; do
+# Wait for the Docker daemon to start. Redirects output to appended logs, and "2>&1" writes errors to same as output
+while ! docker info >> /usr/src/app/logs/createDockerLog.log 2>&1 ; do
     echo "...WAITING for Docker daemon to start..."
     sleep 1
 done
 
 echo "...OPENING LISTENER..."
 
+sh /usr/src/app/service/cleaner.sh >> /usr/src/app/logs/cleaningLog.log 2>&1 &
+
 # Runs listener-script to start watching shared folder
-sh /usr/src/app/listener.sh
+sh /usr/src/app/service/listener.sh
 
 echo "...ENTRYPOINT END..."
